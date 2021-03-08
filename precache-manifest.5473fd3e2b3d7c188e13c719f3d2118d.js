b0b6fc24b1c3175414984682586174335879b97d
alicereactus <agamia@mail.ru>
2021-03-08 19:13:12 +0300

    Updates

--- precache-manifest.5473fd3e2b3d7c188e13c719f3d2118d.js
+++ precache-manifest.5473fd3e2b3d7c188e13c719f3d2118d.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ef186fc72ed3b4dd73879d57bb4c03cc",
+    "revision": "ec36e2fce839a8e05f380825ded19b53",
     "url": "/project-todolist-ts/index.html"
   },
   {
-    "revision": "b95741e384ddb53646f2",
+    "revision": "de3b32cfd186f34380f9",
     "url": "/project-todolist-ts/static/css/main.c9c3dcf0.chunk.css"
   },
   {
-    "revision": "b77e5f7aec0024583705",
-    "url": "/project-todolist-ts/static/js/2.810d14cd.chunk.js"
+    "revision": "76e3ab7fdcd541f7ab41",
+    "url": "/project-todolist-ts/static/js/2.0994a198.chunk.js"
   },
   {
     "revision": "d2266803016540034565984c8bb59069",
-    "url": "/project-todolist-ts/static/js/2.810d14cd.chunk.js.LICENSE.txt"
+    "url": "/project-todolist-ts/static/js/2.0994a198.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b95741e384ddb53646f2",
-    "url": "/project-todolist-ts/static/js/main.022ddc81.chunk.js"
+    "revision": "de3b32cfd186f34380f9",
+    "url": "/project-todolist-ts/static/js/main.129a69e3.chunk.js"
   },
   {
     "revision": "1f4f557f2ac8ab988d00",
