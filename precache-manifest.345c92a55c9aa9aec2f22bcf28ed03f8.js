a5abccdc358bc6f98b436716fd50aa9ecae5469d
alicereactus <agamia@mail.ru>
2021-03-22 23:59:04 +0300

    Updates

--- precache-manifest.345c92a55c9aa9aec2f22bcf28ed03f8.js
+++ precache-manifest.345c92a55c9aa9aec2f22bcf28ed03f8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d28d4c41b1766df2f663171cd73b3b9d",
+    "revision": "7d182e114a147977168f2e9352366447",
     "url": "/project-todolist-ts/index.html"
   },
   {
-    "revision": "978484a945dababcf9e5",
+    "revision": "d52fd33304b89b4cfb18",
     "url": "/project-todolist-ts/static/css/main.c9c3dcf0.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/project-todolist-ts/static/js/2.d56c83c1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "978484a945dababcf9e5",
-    "url": "/project-todolist-ts/static/js/main.90aa7519.chunk.js"
+    "revision": "d52fd33304b89b4cfb18",
+    "url": "/project-todolist-ts/static/js/main.16d09a85.chunk.js"
   },
   {
     "revision": "1f4f557f2ac8ab988d00",
